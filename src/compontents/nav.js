44b6808c61a9998d92467e0d19370a1a2b66e48c
besnikristemi <niki.ristemi9@gmail.com>
2024-01-24 18:10:39 +0100

    ndrusha

--- src/compontents/nav.js
+++ src/compontents/nav.js
@@ -1,27 +1,29 @@
-'use client'
+"use client";
 import Image from "next/image";
 export default function Nav() {
-    
-    
-    const navLink = [
-        {href: '/', label: 'Home'},
-        {href: '/about', label: 'About '},
-        {href: '/contact', label: 'Contact'}
-    ];
+  const navLink = [
+    { href: "/", label: "Home" },
+    { href: "/about", label: "About " },
+    { href: "/contact", label: "Contact" },
+  ];
 
-    
-    return(
+  return (
+    <div className="flex justify-between items-center py-4">
+      {/* LOGO */}
+      <div className="">
+        <Image src="/images/logo.png" width={200} height={200} alt="logo" />
+      </div>
+      {/* NAV LINKS */}
 
-    <div>
-        {/* LOGO */}
-        <div>
-        <Image src="/images/logo.png" width={200} height={200} alt="logo"/>
-        </div>
-        {/* NAV LINKS */}
-        <div>
-
-        </div>
+      <div className="flex items-center">
+        <ul className="flex items-center gap-16">
+          {navLink.map((link) => (
+            <li key={link.href} className="text-2xl font-bold bg-gray-500 px-8 py-4">
+              <a href={link.href}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    )
-    
+  );
 }
